23ac5e6ef67b7f9dd231566370febc6728141609
Vanessa <141968886+petrihcour@users.noreply.github.com>
2023-08-17 16:04:17 -0500

    Create template-literals.js
    how to combine strings with template literals, while also using different string methods like .join()

---
+++ strings-and-template-literals/template-literals.js
@@ -0,0 +1,37 @@
+/*
+  Complete the function below according to the instructions.
+  
+  When the function's parameters reference `products`, they are referencing an array of objects. Each object has the following shape:
+   {
+     name: "Slip Dress",
+     priceInCents: 8800,
+     availableSizes: [ 0, 2, 4, 6, 10, 12, 16 ]
+   }
+*/
+
+function listAllItems(products) {
+  if (products.length === 0) {
+    return "There are no items for sale."
+  }
+
+  if (products.length === 1) {
+    return `There is 1 item for sale: ${products[0].name}.`;
+  }
+
+  if (products.length === 2) {
+    products.join("");
+    return `There are 2 items for sale: ${products[0].name} and ${products[1].name}.`;
+  }
+
+  if (products.length > 2) {
+    const items = [];
+    for (let i = 0; i < products.length; i++) {
+      items.push(products[i].name);
+    }
+    return `There are ${products.length} items for sale: ${items.join(", ")}.`;
+  }
+}
+
+module.exports = {
+  listAllItems,
+};
